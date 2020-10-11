698b2b625d70aa8d71a9a1ec3df1e35a8c5a7fbf
Jeames Aitkin <Jeames8kin@hotmail.com>
2020-10-11 13:50:10 +1100

    v1.0a: More build files

---
+++ rust/backup_script/target/debug/build/libc-47993371f29a30df/build_script_build-47993371f29a30df.d
@@ -0,0 +1,5 @@
+/home/jeames8kin/LinuxBackup_Rust2/rust/backup_script/target/debug/build/libc-47993371f29a30df/build_script_build-47993371f29a30df: /home/jeames8kin/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.79/build.rs
+
+/home/jeames8kin/LinuxBackup_Rust2/rust/backup_script/target/debug/build/libc-47993371f29a30df/build_script_build-47993371f29a30df.d: /home/jeames8kin/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.79/build.rs
+
+/home/jeames8kin/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.79/build.rs:
---
+++ rust/backup_script/target/debug/deps/backup_script-d2202787e1e5cb14.d
@@ -0,0 +1,5 @@
+/home/jeames8kin/LinuxBackup_Rust2/rust/backup_script/target/debug/deps/backup_script-d2202787e1e5cb14.rmeta: src/main.rs
+
+/home/jeames8kin/LinuxBackup_Rust2/rust/backup_script/target/debug/deps/backup_script-d2202787e1e5cb14.d: src/main.rs
+
+src/main.rs:
