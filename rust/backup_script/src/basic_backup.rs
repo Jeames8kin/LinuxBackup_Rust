768d21d18eda7f71c06c084eea7dd6460965ddc6
Jeames Aitkin <Jeames8kin@hotmail.com>
2020-10-11 19:08:17 +1100

    Nothing basically

--- rust/backup_script/src/basic_backup.rs
+++ rust/backup_script/src/basic_backup.rs
@@ -1,3 +1,16 @@
+use std::process::Command;
 pub fn basic_backup() {
     println!("basic backup test");
+
+    Command::new("ls")
+        .arg("-l")
+        .arg("-a")
+        .spawn()
+        .expect("ls command failed to start");
+        
+        
+
 }
+
+
+
