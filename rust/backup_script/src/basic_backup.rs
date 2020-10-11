e070062cf6929df971cf1a311675dbc82318f1a1
Jeames Aitkin <Jeames8kin@hotmail.com>
2020-10-11 23:21:40 +1100

    v1.01b: Everything's a bit borked right now

--- rust/backup_script/src/basic_backup.rs
+++ rust/backup_script/src/basic_backup.rs
@@ -2,15 +2,20 @@ use std::process::Command;
 pub fn basic_backup() {
     println!("basic backup test");
 
-    Command::new("ls")
+/*    let output = Command::new("ls")
         .arg("-l")
         .arg("-a")
         .spawn()
-        .expect("ls command failed to start");
+        .expect("ls command failed to start"); */
         
-        
+        let pacman_command = "pacman -Qqe | tr '\n' ' '";
 
-}
+        let pacman_output = Command::new(pacman_command)
+        
+        .spawn()
+        .expect("pacman failed to reply.");
+
+} 
 
 
 
