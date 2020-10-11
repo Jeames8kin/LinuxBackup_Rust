58556d9d1e864a6ab34db6896ce0b90cf305c33f
Jeames Aitkin <Jeames8kin@hotmail.com>
2020-10-11 14:04:01 +1100

    v1.0b: Working out .gitignore and ignoring folders

--- rust/backup_script/src/main.rs
+++ rust/backup_script/src/main.rs
@@ -1,19 +1,19 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
 
 let title = "Backup and restore thing";    
 let version = "Current version: v1.0";
 let disclaimer = "Note: This program is subject to change at anytime.";
-let selection1 = "Select your option you want to run:";
 
 println!("{}\n", title);
 println!("{}", version);
 println!("{}\n", disclaimer);
 
-println!("{}", selection1);
+println!("Select your option you want to run:");
 println!("1. Backup the current user (basic, no other input really required)");
 println!("2. Backup the current user (advanced, more options)");
-println!("3. Restore from the most recent backup in the cloud (will check against current packages before beginning)");
+println!("3. Restore from the most recent backup in the repository/branch (will check against current packages before beginning)");
 
 }
