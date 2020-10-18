535dc20c5c7be24edc87df3cb309f6b61fa816dc
Jeames Aitkin <Jeames8kin@hotmail.com>
2020-10-18 20:27:11 +1100

    Some more changes before versioning begins

--- rust/backup_script/src/basic_backup.rs
+++ rust/backup_script/src/basic_backup.rs
@@ -1,4 +1,7 @@
 use std::process::Command;
+
+extern crate os_type;
+
 pub fn basic_backup() {
     println!("basic backup test");
 
@@ -17,12 +20,36 @@ pub fn basic_backup() {
         .expect("pacman failed to reply.");
 */
 
-    let output = if cfg!(target_os = "windows") { //Checks if the host is Windows or Linux
-        println!("Run this on an Arch derivative, otherwise this will not run!")
-
+/*    let output = if cfg!(target_os = "windows") { //Checks if the host is Windows or Linux
+        println!("Run this on an Arch derivative, otherwise this will not run!");
+        std::process::exit(1);
     } else {
         println!("Checked host OS: Linux")
-    };
+    }; */
+
+    let _os = os_type::current_platform();
+    match os_type::current_platform().os_type {
+        os_type::OSType::Arch => {
+            backup_start();
+        } 
+        os_type::OSType::Manjaro => {
+            println!("Please note that using Manjaro may have slightly out of date packages compared to Arch, so some things may not work.");
+            backup_start();
+        }
+        _ => {
+            println!("This operating system is invalid, please use Arch or Manjaro!");
+            std::process::exit(1);
+        }
+    }
+    
+
+    fn backup_start() {
+        println!("Beginning backup...")
+
+        
+
+    
+    }
 
 } 
 
