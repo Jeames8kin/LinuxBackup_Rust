8d0ff233d1fc60b4f8354157d2be800bb4b65ebe
Jeames Aitkin <Jeames8kin@hotmail.com>
2020-10-18 17:48:42 +1100

    Removed build files

--- rust/backup_script/target/debug/backup_script.d
+++
@@ -1,1 +0,0 @@
-/home/jeames8kin/LinuxBackup_Rust2/rust/backup_script/target/debug/backup_script: /home/jeames8kin/LinuxBackup_Rust2/rust/backup_script/src/main.rs
--- rust/backup_script/target/rls/debug/deps/backup_script-2e190c7f915a66e4.d
+++
@@ -1,5 +0,0 @@
-/home/jeames8kin/LinuxBackup_Rust2/rust/backup_script/target/rls/debug/deps/backup_script-2e190c7f915a66e4.rmeta: src/main.rs
-
-/home/jeames8kin/LinuxBackup_Rust2/rust/backup_script/target/rls/debug/deps/backup_script-2e190c7f915a66e4.d: src/main.rs
-
-src/main.rs:
